2c42643f2504c44fef5c57c864a017e4b2caafe6
Egor <thewarriors793@mail.ru>
2024-09-01 22:29:32 +0300

    123

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout # Аутефикация в джанго
+from django.contrib import messages # выходят сообщения на экран при регистрации
+
 
 def home(request):
     products = Product.objects.all() # добовляем модель продукта и присваем ему имя products
@@ -12,8 +15,26 @@ def about_shop(request):
     return render(request, 'about_shop.html')
 
 
-def about_shop(request):
-    return render(request, 'about_shop.html')
+def login_user(request): # регистрация пользователя
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,('Cool!!'))
+            return redirect('home')
+        else:
+            messages.success(request, ('Cool!!'))
+            return redirect('login')
 
+    else:
+        return render(request, 'login.html',{})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('You have been logged out'))
+    return redirect('home') # redirect перенаправляет на страницу home.html
 
 
